0c0a1ea1f854c34506edf29095ae0bb44257807b
R-NIRMALKUMAR <nirmalkumarprojects@gmail.com>
2023-03-10 23:55:12 +0530

    added notebooks

--- setup.py
+++ setup.py
@@ -8,11 +8,11 @@ def get_requirements(file_path):
 
 	return requirements
 
-setup(
-	name = "MLProject",
-	version = "0.0.1",
-	author = "R-NIRMALKUMAR",
-	author_email = "nirmalkumarprojects@gmail.com",
-	packages = find_packages(),
-	install_requires = get_requirements("requirements.txt")
-)
+# setup(
+# 	name = "MLProject",
+# 	version = "0.0.1",
+# 	author = "R-NIRMALKUMAR",
+# 	author_email = "nirmalkumarprojects@gmail.com",
+# 	packages = find_packages(),
+# 	install_requires = get_requirements("requirements.txt")
+# )
